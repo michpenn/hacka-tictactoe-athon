43e4c949886069f507a067a6d2a34686e60b7f99
michpenn <michpenn@icloud.com>
2015-11-04 14:03:41 -0800

    winning arrays found

--- tictactoe.js
+++ tictactoe.js
@@ -4,7 +4,8 @@
 var celllabel;
 var cellvalues = [];
 var cellRows = [];
-var cellColumns =[];
+var cellColumns = [];
+var cellDiagonals = [];
 var winningArrays = [];
 var NumberOfRows;
 
@@ -14,11 +15,9 @@ function makeCellLabels(rows) {
 
     for (var i = 0; i < boardsize; i++) {
         celllabel = Math.pow(2, i);
-        console.log(celllabel);
         cellvalues.push(celllabel);
 
     }
-    console.log('these are the values of the cells: ', cellvalues);
 }
 
 makeCellLabels(4);
@@ -27,37 +26,46 @@ function generateWins(cellvalues) {
     var rowStartMarker = 0;
     var rowEndMarker = NumberOfRows - 1;
     var rowArray = [];
-    var columnArray =[];
+    var columnArray = [];
+    var diagonalArray = [];
+    var diagonalArray2 = [];
     for (var j = 0; j < cellvalues.length; j++) {
         //this makes the row arrays
         if (cellvalues[j] == cellvalues[rowStartMarker]) {
-            console.log('this will be the start of a row, at index: ', j);
             for (var k = j; k <= rowEndMarker; k++) {
                 rowArray.push(cellvalues[k]);
             }
             cellRows.push(rowArray);
             rowArray = [];
             //This makes the column arrays
-            for (var m=j; m<NumberOfRows; m++) {
-                columnArray.push(cellvalues[m], cellvalues[m+NumberOfRows], cellvalues[m+NumberOfRows+NumberOfRows]);
+            for (var m = j; m < NumberOfRows; m++) {
+                for (var n = j; n < NumberOfRows; n++) {
+                    if (k == NumberOfRows && m == 1) {
+                        //this makes the diagonals
+                        var diagonal = ((k + m) * n);
+                        var diagonal2 = ((k - m) * (n + 1));
+                        diagonalArray.push(cellvalues[diagonal]);
+                        diagonalArray2.push(cellvalues[diagonal2]);
+                    }
+                    columnArray.push(cellvalues[m + (n * NumberOfRows)])
+                }
                 cellColumns.push(columnArray);
-                columnArray=[];
+                columnArray = [];
             }
 
             rowStartMarker += NumberOfRows;
         }
         else if (cellvalues[j] == cellvalues[rowEndMarker]) {
-            console.log('this will be the end of a row, at index: ', j);
             rowEndMarker += NumberOfRows;
 
         }
-        else {
-            console.log(j);
-        }
     }
+    cellDiagonals.push(diagonalArray, diagonalArray2);
     console.log('winning rows: ', cellRows);
     console.log('winning columns: ', cellColumns);
-    winningArrays.push(cellRows, cellColumns);
+    console.log('winning diagonal: ', cellDiagonals);
+    winningArrays.push(cellRows, cellColumns, cellDiagonals);
+    console.log('winning options: ', winningArrays);
 }
 
 generateWins(cellvalues);
